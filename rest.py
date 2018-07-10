29b8ee4bd7d74dd44aaa2281ef7fa336a729a672
Felix Hellman <felix.hellman@trustly.com>
2018-07-10 18:05:39 +0200

    pushing from capi

--- rest.py
+++ rest.py
@@ -5,13 +5,16 @@ from json import dumps
 from time import sleep, time
 import flask
 import random
+from flask_cors import CORS
 from gpiozero import LED
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
+cors = CORS(app, resources={r"/api/*":{"origins":"*"}})
 
 pump_pins = [LED(17),LED(22),LED(27)]
-status = [0,0,0] 
+status = [0,0,0]
 
 cl_per_sec = 0.1
 
@@ -29,12 +32,16 @@ def pump_by_liquid(liquid):
         return pump_by_liquid[liquid]
 
 def clear():
+    enablePumps()
+    sleep(20)
+    disablePumps()
+
+def enablePumps():
     for i, v in enumerate(pump_pins):
         v.on()
         status[i] = 1
 
-    sleep(20)
-
+def disablePumps():
     for i, v in enumerate(pump_pins):
         v.off()
         status[i] = 0
@@ -58,34 +65,45 @@ def isAvailable():
 	    return False
     return True
 
-def demoRecipe():
-    r = recipe('vodka cranberry')
-    start = time()
-    total_time = secs_to_pour(reduce(max, r.values()))
-
-    for ingredient in r:
-        enablePump(ingredient)
-
-    while (time() - start) < total_time:
-        per_tube_cl_poured = cl_per_sec * (time() - start)
+def pour(drink_name):
+    if isAvailable:
+        r = recipe(drink_name)
+        start = time()
+        total_time = secs_to_pour(reduce(max, r.values()))
 
         for ingredient in r:
-            if r[ingredient] - per_tube_cl_poured <= 0:
-                disablePump(ingredient)
+            enablePump(ingredient)
+            sleep(5)
 
-        sleep(0.2)
+        while (time() - start) < total_time:
+            per_tube_cl_poured = cl_per_sec * (time() - start)
 
-    for ingredient in r:
-        disablePump(ingredient)
+            for ingredient in r:
+                if r[ingredient] - per_tube_cl_poured <= 0:
+                    disablePump(ingredient)
+
+           # sleep(0.2)
+
+        for ingredient in r:
+            disablePump(ingredient)
 
 class Switch_On(Resource):
     def get(self):
         result = ""
         if isAvailable():
             result = {'state':'Starting'}
-            enablePump(0)
-            enablePump(1)
-            enablePump(2)
+            enablePumps()
+        else:
+            result = {'state':'Busy'}
+        return flask.jsonify(result)
+
+class Switch_Pump(Resource):
+    def get(self):
+        result = ""
+        if isAvailable():
+            result = {'state':'Starting'}
+            status[0] = 1
+            pump_pins[0].on()
         else:
             result = {'state':'Busy'}
         return flask.jsonify(result)
@@ -93,9 +111,7 @@ class Switch_On(Resource):
 class Switch_Off(Resource):
     def get(self):
         result = {'state':'Stopping'}
-        disablePump(0)
-        disablePump(1)
-        disablePump(2)
+        disablePumps()
         return flask.jsonify(result)
 
 class GetCurrentStatus(Resource):
@@ -112,17 +128,51 @@ class DemoRecipe(Resource):
    def get(self):
       result = ""
       if isAvailable():
-         demoRecipe()
+         pour("trustly special")
+         result = {'state':'Drink is ready'}
+      else:
+         result = {'state':'Busy'}
+      return flask.jsonify(result)
+
+class CranberryVodka(Resource):
+   def get(self):
+      result = ""
+      if isAvailable():
+         pour("cranberry vodka")
+         result = {'state':'Drink is ready'}
+      else:
+         result = {'state':'Busy'}
+      return flask.jsonify(result)
+
+class Vodka(Resource):
+   def get(self):
+      result = ""
+      if isAvailable():
+         pour("vodka")
+         result = {'state':'Drink is ready'}
+      else:
+         result = {'state':'Busy'}
+      return flask.jsonify(result)
+
+class Cranberry(Resource):
+   def get(self):
+      result = ""
+      if isAvailable():
+         pour("cranberry")
          result = {'state':'Drink is ready'}
       else:
          result = {'state':'Busy'}
       return flask.jsonify(result)
 
 api.add_resource(Switch_On, '/on')
+api.add_resource(Switch_Pump, '/pump')
 api.add_resource(Switch_Off,'/off')
 api.add_resource(GetCurrentStatus,'/status')
 api.add_resource(Clear,'/clear')
 api.add_resource(DemoRecipe,'/demo')
+api.add_resource(CranberryVodka,'/cranberryvodka')
+api.add_resource(Vodka,'/vodka')
+api.add_resource(Cranberry,'/cranberry')
 
 if __name__ == '__main__':
     app.run(port='2000', host='0.0.0.0')
